044de45ff563f074692870f163a2e25e5e2282fc
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-02-11 09:44:53 -0500

    eh

---
+++ main.js
@@ -0,0 +1,26 @@
+const head = document.querySelector('h3');
+head.style.color = "purple";
+
+const p = document.querySelector('p');
+p.style.fontSize = '200px';
+
+
+const links = document.querySelector("ul");
+links.style.transform = "rotate(150deg)";
+
+const things = document.querySelector("ol");
+things.style.transform = "rotate(25deg)";
+
+const it = document.querySelector("h2");
+it.style.color = "red";
+it.style.wordSpacing = "90px";
+
+const there = document.querySelector("h4");
+there.style.color = "green";
+there.style.wordSpacing = "90px";
+there.style.transform = "rotate(250deg)";
+
+const here = document.title;
+here.style.color = 'green';
+
+
